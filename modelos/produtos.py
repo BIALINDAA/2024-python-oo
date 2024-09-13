db9eebd593532917036bcb17f53eda9e434bdfe2
bibia <134613103+BIALINDAA@users.noreply.github.com>
2024-09-13 07:47:21 -0300

    Add files via upload

--- modelos/produtos.py
+++ modelos/produtos.py
@@ -1,18 +1,22 @@
 class Cliente:
+    clientes = []
+
     def __init__(self, nome, servico, horario, pagamento):
         self.nome = nome
         self.servico = servico
         self.horario = horario
         self.pagamento = pagamento
+        Cliente.clientes.append(self)
 
     def __str_(self):
         return f'{self.nome} | {self.servico} | {self.horario} | {self.pagamento}'
+
+    def listar_cliente():
+        for cliente in Cliente.clientes:
+            print(f'{cliente.nome} | {cliente.servico} | {cliente.horario} | {cliente.pagamento}')
     
 cliente_Maria = Cliente('Maria', 'Corte de cabelo', '10:00', 'Dinheiro')
 cliente_Fernanda = Cliente('Fernanda', 'Manicure', '14:00', 'Cartão')
 cliente_Ana = Cliente('Ana', 'Corte de cabelo', '11:00', 'Dinheiro')
 
-clientes = [cliente_Maria, cliente_Fernanda, cliente_Ana]
-
-print(cliente_Maria)
-print(cliente_Fernanda)
+Cliente.listar_cliente()
